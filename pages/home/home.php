fc95ba572f8a16dd8a8b39930a2e5aaf6e329a0d
astafievK <AstafevK@yandex.ru>
2023-10-13 10:33:23 +0300

    main

---
+++ pages/home/home.php
@@ -0,0 +1,287 @@
+<?php
+require "../connection.php"
+
+?>
+
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Главная</title>
+</head>
+<body>
+<div class="table-container">
+    <table>
+        <thead>
+            <tr>
+                <th class="column-id"></th>
+                <th class="column-fullName">ФИО</th>
+                <th>ЛР1</th>
+                <th>ЛР2</th>
+                <th>ЛР3</th>
+                <th>ЛР4</th>
+                <th>ЛР5</th>
+                <th>ЛР6</th>
+                <th>ПР1</th>
+                <th>ПР2</th>
+                <th>ПР2</th>
+                <th>ПР2</th>
+                <th>ПР2</th>
+                <th>ПР2</th>
+                <th>ПР2</th>
+                <th>ПР2</th>
+                <th>ПР2</th>
+                <th>ПР2</th>
+                <th>ПР2</th>
+                <th>ПР2</th>
+                <th>ПР2</th>
+                <th>ПР2</th>
+                <th>ПР2</th>
+                <th>ПР2</th>
+                <th>ПР2</th>
+                <th>ПР2</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>15</td>
+                <td>Астафьев Кирилл Александрович</td>
+                <td><div class="satisfactory">3</div></td>
+                <td><div class="cool">5</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="cool">2</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+            </tr>
+
+            <tr>
+                <td>1</td>
+                <td>Астафьев Кирилл Александрович</td>
+                <td><div class="satisfactory">3</div></td>
+                <td><div class="cool">5</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="cool">2</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+            </tr>
+
+            <tr>
+                <td>1</td>
+                <td>Астафьев Кирилл Александрович</td>
+                <td><div class="satisfactory">3</div></td>
+                <td><div class="cool">5</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="cool">2</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+            </tr>
+
+            <tr>
+                <td>1</td>
+                <td>Астафьев Кирилл Александрович</td>
+                <td><div class="satisfactory">3</div></td>
+                <td><div class="cool">5</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="cool">2</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+            </tr>
+
+            <tr>
+                <td>1</td>
+                <td>Астафьев Кирилл Александрович</td>
+                <td><div class="satisfactory">3</div></td>
+                <td><div class="cool">5</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="cool">2</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+            </tr>
+
+            <tr>
+                <td>1</td>
+                <td>Астафьев Кирилл Александрович</td>
+                <td><div class="satisfactory">3</div></td>
+                <td><div class="cool">5</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="cool">2</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+            </tr>
+
+            <tr>
+                <td>1</td>
+                <td>Астафьев Кирилл Александрович</td>
+                <td><div class="satisfactory">3</div></td>
+                <td><div class="cool">5</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="cool">2</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+            </tr>
+
+            <tr>
+                <td>1</td>
+                <td>Астафьев Кирилл Александрович</td>
+                <td><div class="satisfactory">3</div></td>
+                <td><div class="cool">5</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="cool">2</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+            </tr>
+
+            <tr>
+                <td>1</td>
+                <td>Астафьев Кирилл Александрович</td>
+                <td><div class="satisfactory">3</div></td>
+                <td><div class="cool">5</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="cool">2</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+            </tr>
+
+            <tr>
+                <td>1</td>
+                <td>Астафьев Кирилл Александрович</td>
+                <td><div class="satisfactory">3</div></td>
+                <td><div class="cool">5</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="cool">2</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+            </tr>
+
+            <tr>
+                <td>1</td>
+                <td>Астафьев Кирилл Александрович</td>
+                <td><div class="satisfactory">3</div></td>
+                <td><div class="cool">5</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="cool">2</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+            </tr>
+
+            <tr>
+                <td>1</td>
+                <td>Астафьев Кирилл Александрович</td>
+                <td><div class="satisfactory">3</div></td>
+                <td><div class="cool">5</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="cool">2</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+            </tr>
+
+            <tr>
+                <td>1</td>
+                <td>Астафьев Кирилл Александрович</td>
+                <td><div class="satisfactory">3</div></td>
+                <td><div class="cool">5</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="cool">2</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+            </tr>
+
+            <tr>
+                <td>1</td>
+                <td>Астафьев Кирилл Александрович</td>
+                <td><div class="satisfactory">3</div></td>
+                <td><div class="cool">5</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="cool">2</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+            </tr>
+
+            <tr>
+                <td>1</td>
+                <td>Астафьев Кирилл Александрович</td>
+                <td><div class="satisfactory">3</div></td>
+                <td><div class="cool">5</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="cool">2</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+            </tr>
+
+            <tr>
+                <td>1</td>
+                <td>Астафьев Кирилл Александрович</td>
+                <td><div class="satisfactory">3</div></td>
+                <td><div class="cool">5</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="cool">2</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+            </tr>
+
+            <tr>
+                <td>1</td>
+                <td>Астафьев Кирилл Александрович</td>
+                <td><div class="satisfactory">3</div></td>
+                <td><div class="cool">5</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="cool">2</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+            </tr>
+
+            <tr>
+                <td>1</td>
+                <td>Астафьев Кирилл Александрович</td>
+                <td><div class="satisfactory">3</div></td>
+                <td><div class="cool">5</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="bad">2</div></td>
+                <td><div class="cool">2</div></td>
+                <td><div class="good">4</div></td>
+                <td><div class="bad">2</div></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
