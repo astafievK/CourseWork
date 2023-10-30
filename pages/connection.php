b3dc0c6aea0d8d36494229ee3e16610ebdd9ca47
astafievK <AstafevK@yandex.ru>
2023-10-30 22:22:59 +0300

    table v2

---
+++ pages/connection.php
@@ -0,0 +1,8 @@
+<?php
+
+$servername = "localhost";
+$user = "root";
+$password = "root";
+$dbname = "LabWorks";
+
+$conn = mysqli_connect($servername, $user, $password, $dbname);
---
+++ pages/home/scripts/theme.js
@@ -0,0 +1,7 @@
+document.getElementById("btnSwitchTheme").addEventListener("click", function() {
+    document.body.classList.toggle("dark");
+});
+
+document.getElementById("btnMobileSwitchTheme").addEventListener("click", function() {
+    document.body.classList.toggle("dark");
+});
