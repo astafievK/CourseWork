b8ad93d5472841555245b934927359a1c8fd09e5
astafievK <AstafevK@yandex.ru>
2023-10-30 19:10:37 +0300

    nothing

--- pages/home/scripts/theme.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("btnSwitchTheme").addEventListener("click", function() {
-    document.body.classList.toggle("light");
-    document.body.classList.toggle("dark");
-});
-
-document.getElementById("btnMobileSwitchTheme").addEventListener("click", function() {
-    document.body.classList.toggle("light");
-    document.body.classList.toggle("dark");
-});
