4bd2e3e83096f4a7419e93dade44611010aa00aa
astafievK <AstafevK@yandex.ru>
2023-10-31 22:46:08 +0300

    header

--- pages/home/scripts/theme.js
+++ pages/home/scripts/theme.js
@@ -1,7 +1,13 @@
-document.getElementById("btnSwitchTheme").addEventListener("click", function() {
-    document.body.classList.toggle("dark");
-});
+const btnSwitchTheme = document.getElementById("btnSwitchTheme");
+const body = document.body;
+const themeImage = document.getElementById("themeImage");
 
-document.getElementById("btnMobileSwitchTheme").addEventListener("click", function() {
-    document.body.classList.toggle("dark");
+btnSwitchTheme.addEventListener("click", function() {
+    if (body.classList.contains("dark")) {
+        body.classList.remove("dark");
+        themeImage.src = "../../assets/images/nav/desktop/theme-moon.png";
+    } else {
+        body.classList.add("dark");
+        themeImage.src = "../../assets/images/nav/desktop/theme-sun.png";
+    }
 });
