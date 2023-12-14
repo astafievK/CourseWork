77f2829ad4a4e5ec8be34fb54780e59d831b5533
astafievK <AstafevK@yandex.ru>
2023-12-14 20:20:38 +0300

    update

--- src/components/PageDisciplines/PageDisciplines.tsx
+++ src/components/PageDisciplines/PageDisciplines.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import StaffHeaderDisciplines from "../StaffHeaderDisciplines/StaffHeaderDisciplines.tsx";
 import TableDisciplines from "../Disciplines/TableDisciplines/TableDisciplines.tsx";
+import {useTypedSelector} from "../../store/hooks/redux.ts";
+import {Navigate} from "react-router-dom";
 
 interface PageDisciplinesProps {
 
 }
 
 const PageDisciplines: React.FC<PageDisciplinesProps> = () => {
+    const {user} = useTypedSelector(state => state.auth)
+
+    if(!user){
+        return <Navigate to="/"/>
+    }
+
+    if(user.role.name === "Student"){
+        return <Navigate to="/stats"/>
+    }
+
     return(
         <>
             <StaffHeaderDisciplines/>
--- src/components/PageStudents/PageStudents.tsx
+++ src/components/PageStudents/PageStudents.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import StaffHeaderStudents from "../StaffHeaderStudents/StaffHeaderStudents.tsx";
 import TableStudents from "../Students/TableStudents/TableStudents.tsx";
+import {useTypedSelector} from "../../store/hooks/redux.ts";
+import {Navigate} from "react-router-dom";
 
 interface PageStudentsProps {
 
 }
 
 const PageStudents: React.FC<PageStudentsProps> = () => {
+    const {user} = useTypedSelector(state => state.auth)
+
+    if(!user){
+        return <Navigate to="/"/>
+    }
+
+    if(user.role.name === "Student"){
+        return <Navigate to="/stats"/>
+    }
+
     return(
         <>
             <StaffHeaderStudents/>
