3a5b175bfb6fc5e2aa690cc753f94d5e54e0453d
astafievK <AstafevK@yandex.ru>
2024-02-02 19:41:34 +0300

    cache fix

--- ReactApp/src/api/api.ts
+++ ReactApp/src/api/api.ts
@@ -2,6 +2,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const baseApi = createApi({
     reducerPath: 'api',
+    keepUnusedDataFor: 5,
+    refetchOnFocus: true,
     tagTypes: ['Disciplines', 'Groups', 'Students', 'Works'],
     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5050/api' }),
     endpoints: (builder) => ({
