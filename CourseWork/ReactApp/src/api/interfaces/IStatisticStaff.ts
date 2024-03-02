1bb7ce5a32af24a9d22172288ff84a03c7ddc7c8
astafievK <AstafevK@yandex.ru>
2024-03-02 17:14:56 +0300

    wide project

---
+++ CourseWork/ReactApp/src/api/interfaces/IStatisticStaff.ts
@@ -0,0 +1,11 @@
+interface IStatisticStaff {
+    studentName: string
+    studentSurname: string
+    completedTasks: number[]
+    workTasks: number[]
+    idStudent: number
+    idWork: number
+    percentage: number
+    totalMark: number
+    tasksCount: number
+}
